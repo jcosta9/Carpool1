c90d0f4cc5f9d391b2502fbede383753440d9bce
jcosta9 <jpaulo.caraujo@gmail.com>
2014-10-25 10:49:41 -0200

    tirei o que nao estava sendo utilizado

--- src/com/example/carpool/MainActivity.java
+++ src/com/example/carpool/MainActivity.java
@@ -2,14 +2,10 @@ package com.example.carpool;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.Window;
 import android.widget.Button;
-//import android.widget.EditText;
 import android.view.View.OnClickListener;
 import android.view.View;
-//import android.annotation.SuppressLint;
 import android.content.Intent;
 
 
@@ -40,23 +36,5 @@ public class MainActivity extends ActionBarActivity {
         		);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    /*@Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }*/
+   
 }
